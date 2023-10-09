cfed552af5f9cb1d862b24080c7616d07ca49b37
rodriguezj2 <jorge.rodriguez@dowjones.com>
2023-10-08 23:54:15 -0400

    chore: refactor, read the db credentials, update .env file

--- server/config/db.go
+++ server/config/db.go
@@ -2,10 +2,17 @@ package config
 
 import (
     "database/sql"
+    "fmt"
+    "os"
     _ "github.com/lib/pq"
 )
 
 func Connect() (*sql.DB, error) {
-    connStr := "user=your_user dbname=utesa_database sslmode=disable password=your_password"
+    user := os.Getenv("PGDB_USER")
+    password := os.Getenv("PGDB_PASSWORD")
+    dbname := os.Getenv("PGDB_DATABASE")
+    sslmode := os.Getenv("SSL_MODE")
+
+    connStr := fmt.Sprintf("user=%s dbname=%s sslmode=%s password=%s", user, dbname, sslmode, password)
     return sql.Open("postgres", connStr)
 }
